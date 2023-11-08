a061bd0a23fa0866c06c024f57fdefa71a932f47
justinp24 <justinpardo2423@gmail.com>
2023-11-08 00:46:34 -0500

    Implement function for round selection & winner logic

--- game.js
+++ game.js
@@ -11,3 +11,23 @@ function getplayerchoice() {
     let lowerchoice = userchoice.toLowerCase();
     return lowerchoice;
 }
+
+/// Round Winner Selection 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's A Tie";
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You Win this! Rock beats Scissors";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win this! Scissors beats Paper";
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win this! Paper beats Rock";
+    }
+    else {
+        return "Computer Wins!";
+    }
+}
+
